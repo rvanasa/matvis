3d2c7acd004f6592fcfa54f16e95773c72e80241
Ryan Vandersmith <ryanvandersmith@gmail.com>
2018-02-13 16:16:13 -0700

    Add documentation; improve matvis2 illustration of imaginary numbers

---
+++ README.md
@@ -0,0 +1,12 @@
+# matvis
+
+Finally...it's here...a way to actually visualize and understand matrix multiplication.
+
+This is a very, very new project, so right now I just have a few scripts to render the following:
+
+    matvis2 - basic 2D rotations with imaginary offset ring
+    matvis2i - 2D rotations with an imaginary projection along the Z axis
+    matvis2ni - N-dimensional rotations flattened into 2D with an imaginary Z axis
+    matvisni - N-dimensional rotations flattened into 3D with imaginary offset points
+
+I'll be adding new concepts/features as time goes on. Have fun!
--- matvis2.py
+++ matvis2.py
@@ -7,7 +7,7 @@ from matplotlib import animation
 from cirquit import *
 
 matrix = np.matrix([
-    [0, 1],
+    [0, -1j],
     [1, 0],
 ])
 
@@ -18,13 +18,13 @@ data = np.matrix([[np.cos(t ** 2), np.sin(t ** 2)] for t in np.arange(line_res +
 
 fig = plt.figure()
 plt.axis('equal')
-real, imag = plt.plot(*data.real, *data.imag.T)[0:2]
+imag, real = plt.plot(*data.imag, *data.real)[0:2]
 
 
 def display(t):
     move_data = lin.fractional_matrix_power(matrix, t * anim_speed / 1000) * data
     real.set_data(*move_data.real)
-    imag.set_data(*move_data.imag)
+    imag.set_data(*move_data.real + move_data.imag)
     return [real, imag]
 
 
